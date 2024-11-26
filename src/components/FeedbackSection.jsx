84530134245f4d0d3adb8d80bb6716d06867979b
S.R.K. <monck113@bk.ru>
2024-11-26 18:04:30 +0300

    123

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -1,16 +1,49 @@
+import Button from "./Button/Button";
+import { useState } from "react";
+
 export default function FeedbackSection() {
+  const [name, setName] = useState("");
+  const [reason, setReason] = useState("help");
+
+  function handelNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handelReasonChange(event) {
+    setReason(event.target.value);
+  }
   return (
     <section>
       <h3>Обратная связь</h3>
       <form>
         <label htmlFor="name">Ваше имя</label>
-        <input type="text" className="controle" id="name" />
+        <input
+          type="text"
+          id="name"
+          className="controle"
+          value={name}
+          style={{
+            border: name.trim().length ? null : "1px solid red",
+          }}
+          onChange={handelNameChange}
+        />
         <label htmlFor="reason">Причина обращения</label>
-        <select id="reason" className="controle">
+        <select
+          id="reason"
+          className="controle"
+          value={reason}
+          onChange={handelReasonChange}
+        >
           <option value="error">Ошибка</option>
           <option value="help">Нужна помощь</option>
           <option value="suggest">Предложения</option>
         </select>
+        <pre>
+          Name: {name}
+          <br />
+          Reason: {reason}
+        </pre>
+        <Button>Отправить</Button>
       </form>
     </section>
   );
